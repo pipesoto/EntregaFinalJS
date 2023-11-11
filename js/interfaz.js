8005f0a3601c37e8ed5bd875297e4ac73827467f
Felipe Soto <39935940+pipesoto@users.noreply.github.com>
2023-11-10 23:29:08 -0300

    Modificación para poder hacer funcionar el localStorage

--- js/interfaz.js
+++ js/interfaz.js
@@ -1,29 +1,3 @@
-const compraForm = document.getElementById("compraForm");
-    const categoriaSelect = document.getElementById("categoriaSelect");
-    const productoSelect = document.getElementById("productoSelect");
-    const cantidadInput = document.getElementById("cantidadInput");
-    const tablaCuerpo = document.getElementById("tablaCuerpo");
-
-    compraForm.addEventListener("submit", function (e) {
-        e.preventDefault(); // Evitar que el formulario se envíe
-    });
-
-    categoriaSelect.addEventListener("change", function () {
-        const categoria = categoriaSelect.value;
-
-        const productos = precios[categoria];
-        productoSelect.innerHTML = ""; // Limpiar opciones anteriores
-
-        for (const producto in productos) {
-            const option = document.createElement("option");
-            option.value = producto;
-            option.textContent = producto;
-            productoSelect.appendChild(option);
-        }
-    });
-
-    categoriaSelect.dispatchEvent(new Event('change'));
-
 
 let precios = {}; // Inicializar precios como un objeto vacío
 
@@ -62,3 +36,29 @@ function actualizarOpcionesProductos() {
 
 // Llama a la función para cargar productos cuando se cargue la página
 cargarProductosDesdeJSON();
+
+const compraForm = document.getElementById("compraForm");
+    const categoriaSelect = document.getElementById("categoriaSelect");
+    const productoSelect = document.getElementById("productoSelect");
+    const cantidadInput = document.getElementById("cantidadInput");
+    const tablaCuerpo = document.getElementById("tablaCuerpo");
+
+    compraForm.addEventListener("submit", function (e) {
+        e.preventDefault(); // Evitar que el formulario se envíe
+    });
+
+    categoriaSelect.addEventListener("change", function () {
+        const categoria = categoriaSelect.value;
+
+        const productos = precios[categoria];
+        productoSelect.innerHTML = ""; // Limpiar opciones anteriores
+
+        for (const producto in productos) {
+            const option = document.createElement("option");
+            option.value = producto;
+            option.textContent = producto;
+            productoSelect.appendChild(option);
+        }
+    });
+
+    categoriaSelect.dispatchEvent(new Event('change'));
