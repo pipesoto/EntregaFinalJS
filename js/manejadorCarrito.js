e9400982c68170f494d39c795347cc697ec6afb6
Felipe Soto <39935940+pipesoto@users.noreply.github.com>
2023-11-24 15:46:20 -0300

    Se actualiza el archivo manejadorCarrito.js

--- js/manejadorCarrito.js
+++ js/manejadorCarrito.js
@@ -18,7 +18,7 @@ const carrito = cargarCarritoDesdeLocalStorage();
 
 // Función para verificar la visibilidad del botón de "Pagar" y ocultarlo si no hay productos en el carrito
 
-function agregarAlCarrito() {
+  function agregarAlCarrito() {
   const categoria = categoriaSelect.value;
   const producto = productoSelect.value;
   const cantidad = parseInt(cantidadInput.value);
@@ -28,26 +28,55 @@ function agregarAlCarrito() {
     return;
   }
 
-  const precio = precios[categoria][producto].precio;
-  const precioTotal = precio * cantidad;
-  const item = {
-    categoria: categoria,
-    producto: producto,
-    cantidad: cantidad,
-    precioTotal: precioTotal,
-  };
-  carrito.push(item); // Agregar el producto al carrito
-  const fila = document.createElement("tr");
-  fila.innerHTML = `
-                <td>${producto}</td>
-                <td>${categoria}</td>
-                <td>$${precio}</td>
-                <td>${cantidad}</td>
-                <td><button class="btn btn-danger" onclick="eliminarDelCarrito(this)">Eliminar</button></td>
-            `;
-  fila.setAttribute('data-id', `${producto}-${categoria}`);
-  tablaCuerpo.appendChild(fila);
+  // Verificar si el producto ya está en el carrito
+  const productoExistente = carrito.find(
+    (item) => item.producto === producto && item.categoria === categoria
+  );
+
+  if (productoExistente) {
+    // Si el producto ya está en el carrito, incrementa la cantidad
+    productoExistente.cantidad += cantidad;
+    productoExistente.precioTotal = precios[categoria][producto].precio * productoExistente.cantidad;
+
+  } else {
+    // Si el producto no está en el carrito, lo agrega
+    const precio = precios[categoria][producto].precio;
+    const precioTotal = precio * cantidad;
+    const item = {
+      categoria: categoria,
+      producto: producto,
+      cantidad: cantidad,
+      precioTotal: precioTotal,
+    };
+    carrito.push(item);
+  }
+
+  // Actualiza la tabla 
+  actualizarCarrito();
+}
+
+function actualizarCarrito() {
+  // Limpia la tabla antes de volver a llenarla
+  restablecerTabla();
+
+  // Llena la tabla con los productos actualizados
+  carrito.forEach(item => {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+      <td>${item.producto}</td>
+      <td>${item.categoria}</td>
+      <td>$${precios[item.categoria][item.producto].precio}</td>
+      <td>${item.cantidad}</td>
+      <td><button class="btn btn-danger" onclick="eliminarDelCarrito(this)">Eliminar</button></td>
+    `;
+    fila.setAttribute('data-id', `${item.producto}-${item.categoria}`);
+    tablaCuerpo.appendChild(fila);
+  });
+
+  // Guarda el carrito actualizado en localStorage
   guardarCarritoEnLocalStorage();
+
+  // Muestra la notificación de producto agregado al carrito
   Toastify({
     text: "Se agregó el producto a tu carrito",
     className: "info",
@@ -55,25 +84,22 @@ function agregarAlCarrito() {
       background: "linear-gradient(to right, #00b09b, #96c93d)",
     },
   }).showToast();
+
+
   mostrarBoleta();
 }
-
-
 
 function eliminarDelCarrito(btn) {
   const fila = btn.parentNode.parentNode;
   const producto = fila.getElementsByTagName("td")[0].textContent;
   const categoria = fila.getElementsByTagName("td")[1].textContent;
   const cantidad = parseInt(fila.getElementsByTagName("td")[3].textContent);
-
-  // Ajusta el carrito para reflejar la eliminación del producto
   const itemIndex = carrito.findIndex(
     (item) => item.producto === producto && item.categoria === categoria
   );
   if (itemIndex !== -1) {
     carrito.splice(itemIndex, 1);
   }
-
   precios[categoria][producto].cantidad += cantidad; // Incrementa la cantidad en el objeto de precios
   fila.remove();
   guardarCarritoEnLocalStorage();
@@ -84,7 +110,7 @@ function eliminarDelCarrito(btn) {
       background: "linear-gradient(to right, #f43636, #f43636)",
     },
   }).showToast();
-  mostrarBoleta(); // Actualiza la boleta
+  mostrarBoleta(); 
 }
 function eliminarDelCarritoDesdeBoleta(btn) {
   const fila = btn.parentNode.parentNode;
@@ -113,6 +139,6 @@ function eliminarDelCarritoDesdeBoleta(btn) {
       background: "linear-gradient(to right, #f43636, #f43636)",
     },
   }).showToast();
-  guardarCarritoEnLocalStorage(); // Guardar el carrito actualizado en localStorage
-  mostrarBoleta(); // Actualiza el modal
+  guardarCarritoEnLocalStorage(); 
+  mostrarBoleta(); 
 }
